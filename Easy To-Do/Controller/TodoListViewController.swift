16f9afc63f43a62b4ce3acf4702af9cf6677c553
Nury <nury@nurys-iMac.local>
2018-05-21 15:17:07 +0500

    UI optimisation

--- Easy To-Do/Controller/TodoListViewController.swift
+++ Easy To-Do/Controller/TodoListViewController.swift
@@ -79,9 +79,11 @@ class TodoListViewController: SwipeTableViewController {
             if let color = UIColor(hexString: (selectedCategory!.color))?.darken(byPercentage: CGFloat(indexPath.row) / CGFloat(todoItems!.count)) {
                 cell.backgroundColor = color
                 cell.textLabel?.textColor = ContrastColorOf(color, returnFlat: true)
+                cell.tintColor = ContrastColorOf(color, returnFlat: true)
             }
             
             cell.accessoryType = item.done ? .checkmark : .none
+
         } else {
             cell.textLabel?.text = "No Item Added Yet"
         }
